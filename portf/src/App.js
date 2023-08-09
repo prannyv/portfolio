bb22382862db0f90a19dea3eba49ffe5d5ec69d1
pranav <pranavsworkemail@gmail.com>
2023-08-09 13:47:23 -0400

    created

---
+++ portf/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <body>
+        <p>
+          nonhead
+        </p>
+        </body>
+    </div>
+  );
+}
+
+export default App;
